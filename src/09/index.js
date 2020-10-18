0d7cd84d4d8ff0ca4f0e5fd753e160e5db9c1691
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2020-10-19 07:48:58 +1300

    finished 9th exercise

--- src/09/index.js
+++ src/09/index.js
@@ -1,6 +1,6 @@
-import { createMachine, assign, interpret } from 'xstate';
+import { createMachine, assign, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 const elBody = document.body;
 
 const assignPoint = assign({
@@ -30,6 +30,12 @@ const assignDelta = assign({
   },
 });
 
+const assignXDelta = assign({
+  dx: (context, event) => {
+    return event.clientX - context.px;
+  },
+});
+
 const resetPosition = assign({
   dx: 0,
   dy: 0,
@@ -38,7 +44,7 @@ const resetPosition = assign({
 });
 
 const dragDropMachine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   context: {
     x: 0,
     y: 0,
@@ -52,7 +58,7 @@ const dragDropMachine = createMachine({
       on: {
         mousedown: {
           actions: assignPoint,
-          target: 'dragging',
+          target: "dragging",
         },
       },
     },
@@ -62,6 +68,22 @@ const dragDropMachine = createMachine({
       // that transitions to a "locked" x-axis behavior
       // when the shift key is pressed.
       // ...
+      initial: "normal",
+      states: {
+        normal: {
+          on: {
+            "keydown.shift": "locked",
+          },
+        },
+        locked: {
+          on: {
+            mousemove: {
+              actions: assignXDelta,
+            },
+            "keyup.shift": "normal",
+          },
+        },
+      },
       on: {
         mousemove: {
           actions: assignDelta,
@@ -69,10 +91,10 @@ const dragDropMachine = createMachine({
         },
         mouseup: {
           actions: [assignPosition],
-          target: 'idle',
+          target: "idle",
         },
-        'keyup.escape': {
-          target: 'idle',
+        "keyup.escape": {
+          target: "idle",
           actions: resetPosition,
         },
       },
@@ -83,33 +105,42 @@ const dragDropMachine = createMachine({
 const service = interpret(dragDropMachine);
 
 service.onTransition((state) => {
-  elBox.dataset.state = state.toStrings().join(' ');
+  elBox.dataset.state = state.toStrings().join(" ");
 
   if (state.changed) {
-    elBox.style.setProperty('--dx', state.context.dx);
-    elBox.style.setProperty('--dy', state.context.dy);
-    elBox.style.setProperty('--x', state.context.x);
-    elBox.style.setProperty('--y', state.context.y);
+    elBox.style.setProperty("--dx", state.context.dx);
+    elBox.style.setProperty("--dy", state.context.dy);
+    elBox.style.setProperty("--x", state.context.x);
+    elBox.style.setProperty("--y", state.context.y);
   }
 });
 
 service.start();
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('mousemove', (event) => {
+elBody.addEventListener("mousemove", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('mouseup', (event) => {
+elBody.addEventListener("mouseup", (event) => {
   service.send(event);
 });
 
-elBody.addEventListener('keyup', (e) => {
-  if (e.key === 'Escape') {
-    service.send('keyup.escape');
+elBody.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    service.send("keyup.escape");
+  } else if (e.key === "Shift") {
+    service.send("keyup.shift");
+  }
+  console.log(e.key);
+});
+
+elBody.addEventListener("keydown", (e) => {
+  if (e.key === "Shift") {
+    service.send("keydown.shift");
   }
 });
 
